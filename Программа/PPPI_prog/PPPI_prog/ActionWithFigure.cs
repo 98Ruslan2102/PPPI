ec70afea03fcb857fda17e52ba339ad5e7aa792f
Ruslan Samoilenko <seltia0203@gmail.com>
2023-04-09 00:58:36 +0300

    Создание класса ActionWithFigure

---
+++ Программа/PPPI_prog/PPPI_prog/ActionWithFigure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI_prog
+{
+    /// <summary>
+    /// Класс действий с самой фигурой
+    /// </summary>
+    class ActionWithFigure
+    {
+        /// <summary>
+        /// Конструктор ActionWithFigure
+        /// </summary>
+        public ActionWithFigure()
+        {
+        }
+    }
+}
