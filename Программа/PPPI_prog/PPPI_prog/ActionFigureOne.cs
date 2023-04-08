93a4e0266a2a7c2155d6aa1e75913057353579f6
Ruslan Samoilenko <seltia0203@gmail.com>
2023-04-09 01:01:07 +0300

    Создание класса ActionFigureOne

---
+++ Программа/PPPI_prog/PPPI_prog/ActionFigureOne.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI_prog
+{
+    /// <summary>
+    /// Класс действий с одной вигуры
+    /// </summary>
+    class ActionFigureOne
+    {
+        /// <summary>
+        /// Конструктор ActionFigureOne
+        /// </summary>
+        public ActionFigureOne()
+        {
+        }
+    }
+}
