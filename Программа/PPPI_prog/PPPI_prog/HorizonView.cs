e5162f06e9cae606f85747f8a0d00ae2a930dda6
Ruslan Samoilenko <seltia0203@gmail.com>
2023-04-09 00:30:29 +0300

    Создание класса HorizonView(горизонтальный вид)

---
+++ Программа/PPPI_prog/PPPI_prog/HorizonView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI_prog
+{
+    /// <summary>
+    /// Класс горизонтального вида
+    /// </summary>
+    class HorizonView
+    {
+        /// <summary>
+        /// Конструктор HorizonView
+        /// </summary>
+        public HorizonView()
+        {
+        }
+    }
+}
