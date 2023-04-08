d178fe596213d2f7e141774bf017056baeeb7cee
Ruslan Samoilenko <seltia0203@gmail.com>
2023-04-09 00:36:08 +0300

    Создание класса Shape(фигура, примитив)

---
+++ Программа/PPPI_prog/PPPI_prog/Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPI_prog
+{
+    /// <summary>
+    /// Создание класса Shape(фигура)
+    /// </summary>
+    class Shape
+    {
+        /// <summary>
+        /// Конструктор Shape
+        /// </summary>
+        public Shape()
+        {
+        }
+    }
+}
