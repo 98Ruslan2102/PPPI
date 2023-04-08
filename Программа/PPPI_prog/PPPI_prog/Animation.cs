87180e262932dba816dea4cabf03378946b05ddf
Ruslan Samoilenko <seltia0203@gmail.com>
2023-04-09 00:46:27 +0300

    Добавлен парамерт вид анимации

--- Программа/PPPI_prog/PPPI_prog/Animation.cs
+++ Программа/PPPI_prog/PPPI_prog/Animation.cs
@@ -6,16 +6,20 @@ using System.Threading.Tasks;
 
 namespace PPPI_prog
 {
+    
     /// <summary>
     /// Создание класса Анимации
     /// </summary>
     class Animation
     {
+        string typeAnimation;
         /// <summary>
         /// Конструктор Animation
         /// </summary>
-        public Animation()
+        /// <param name="_typeAnimation"> Вид анимации</param>
+        public Animation(string _typeAnimation)
         {
+            typeAnimation = _typeAnimation;
         }
     }
 }
